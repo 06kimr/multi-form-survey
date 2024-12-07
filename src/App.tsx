411b2bcf36b2f2276b4eb53e17247104f4df518d
rickykim-99 <youngs.kim@creadto.com>
2024-12-08 00:25:59 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import MainLayout from "./components/common/MainLayout";
+import "./App.css";
+import Tabs, {
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+} from "./components/common/Tabs";
+
+function App() {
+  return (
+    <MainLayout>
+      <Tabs>
+        <TabList>
+          <Tab index={0}>tab1</Tab>
+          <Tab index={1}>tab2</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel index={0}>panel1</TabPanel>
+          <TabPanel index={1}>panel2</TabPanel>
+        </TabPanels>
+      </Tabs>
+    </MainLayout>
+  );
+}
+
+export default App;
